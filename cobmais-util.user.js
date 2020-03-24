ef73e8c298218ffd68cb4a2a12d7155059dfe888
romesc <ro_mescua@outlook.com>
2020-03-24 09:57:55 -0300

    v1.0.1

--- cobmais-util.user.js
+++ cobmais-util.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Cobmais - Util
 // @namespace    http://app.cobmais.com.br/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Add IdEvento
 // @author       Rodrigo Mescua
 // @match        http*://app.cobmais.com.br/cob/telecobranca*
 // @match        http*://hmlapp.cobmais.com.br/cob/telecobranca*
 // @match        http*://localhost:*/telecobranca*
+// @match        http*://172.36.1.4:*/telecobranca*
+// @match        http*://192.36.1.4:*/telecobranca*
 // @icon         https://raw.githubusercontent.com/romesc/cobmais-utils/master/favicon.ico
 // @require  http://code.jquery.com/jquery-3.4.1.min.js
 // @require  https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.6/clipboard.min.js
